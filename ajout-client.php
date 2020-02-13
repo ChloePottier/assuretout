caaf5231a95f39b4e49aef9e43092bc1b6df68a0
ChloePottier <chloe.pottier@free.fr>
2020-02-13 09:19:45 +0100

    formulaire ajout client et ajout véhicule

--- ajout-client.php
+++ ajout-client.php
@@ -32,12 +32,6 @@ session_start ();
             $prenom = $_POST['prenom'];   
             $adresse = $_POST['adresse'];
             $tel = $_POST['tel']; 
-            $immatriculation = $_POST['immatriculation'];
-            $marque = $_POST['marque'];
-            $genre = $_POST['genre'];
-            $places = $_POST['nb-places'];
-            $date1ereImmat = $_POST['date-1ere-immat'];
-            $idContrats = $_POST['type'];
             $tab = array(
                 'id' => '',
                 'titre_de_civilite'=>$civilite,
@@ -48,9 +42,25 @@ session_start ();
             $sqlAjoutClient ="INSERT INTO clients VALUES(:id, :titre_de_civilite, :nom, :prenom, :adresse, :tel)";
             $reqAjoutClient = $dbh->prepare($sqlAjoutClient);
             $reqAjoutClient ->execute($tab);
+            if($reqAjoutClient == true ){
+                echo "Le client ".$nom." ".$prenom." a bien été ajouté";
+            } else {
+                echo "Ajout impossible";
+            }
+        } 
+        $sqlClients ="SELECT * FROM clients";
+        $reqClients = $dbh->query($sqlClients);
+        if(isset($_POST['ajouter-vehicule'])){
+            $client = $_POST['client'];
+            $immatriculation = $_POST['immatriculation'];
+            $marque = $_POST['marque'];
+            $genre = $_POST['genre'];
+            $places = $_POST['nb-places'];
+            $date1ereImmat = $_POST['date-1ere-immat'];
+            $idContrats = $_POST['type'];
             $tab2 = array(
                 'plaques_immatriculation' => $immatriculation,
-                'id_client'=> '', //il doit prendre l'id créé avec le client, donc il faudrait créer un 2eme formulaire
+                'id_client'=> $client, 
                 'marque'=> $marque,
                 'genre' => $genre,
                 'nombre_de_places' => $places,
@@ -61,36 +71,47 @@ session_start ();
             $reqAjoutVehicule ->execute($tab2);
             if($reqAjoutVehicule == true ){
                 // header("Location: index.php"); 
-                echo "ca marche";
-                var_dump( $reqAjoutVehicule);
-                var_dump($reqAjoutClient);
+                echo "Le véhicule ".$immatriculation." a bien été ajouté";
             } else {
                 echo "Ajout impossible";
             }
 
         } 
-          ?>  
-        <form method='POST' action='#' class='form-ajout-clients'>
-                    <input name='civilite' type='text' placeholder='Civilité'/> 
-                    <input name='nom' type='text' placeholder='Nom' /> 
-                    <input name='prenom' type='text' placeholder='Prénom' /> 
-                    <input name='adresse' type='text' placeholder='Adresse' /> 
-                    <input name='tel' type='text' placeholder='Téléphone' /> 
-                    <input name='immatriculation' type='text' placeholder="Plaque d'immatriculation" /> 
-                    <input name='marque' type='text' placeholder='Marque de la voiture' /> 
-                    <input name='genre' type='text' placeholder='Genre' /> 
-                    <input name='nb-places' type='number' placeholder='Nombre de places' /> 
-                    <label for="date-1ere-immat">Date de la première immatriculation</label>
-                    <input name='date-1ere-immat' type='date' /> 
-
-                    <select name='type'>
-                        <option value=''>--contrats--</option><?php
-                        while($resContrats=$reqContrats->fetch(PDO::FETCH_ASSOC)){
-                            echo"<option value='".$resContrats['id']."'>".$resContrats['type']."</option>";
-                        };?>
         
-                    </select>
-                    <button type='submit' value='submit' name='ajouter-client' class='btn-modif'>Ajouter client</button></form>
+          ?>  
+        <!-- Formulaire ajouter un client -->
+        <form method='POST' action='#' class='form-ajout-clients'>
+            <input name='civilite' type='text' placeholder='Civilité'/> 
+            <input name='nom' type='text' placeholder='Nom' /> 
+            <input name='prenom' type='text' placeholder='Prénom' /> 
+            <input name='adresse' type='text' placeholder='Adresse' /> 
+            <input name='tel' type='text' placeholder='Téléphone' /> 
+            <button type='submit' value='submit' name='ajouter-client' class='btn-client'>Ajouter le client</button>
+        </form>
+        <!-- Formulaire ajouter un véhicule pour un client -->
+        <form method='POST' action='#' class='form-ajout-vehicule'>
+            <input name='immatriculation' type='text' placeholder="Plaque d'immatriculation" />
+            <select name='client'>
+                <option value=''>--clients--</option><?php
+                while($resClients=$reqClients->fetch(PDO::FETCH_ASSOC)){
+                    echo"<option value='".$resClients['id']."'>".$resClients['nom']." ".$resClients['prenom']."</option>";
+                };?>
+    
+            </select>
+            <input name='marque' type='text' placeholder='Marque de la voiture' /> 
+            <input name='genre' type='text' placeholder='Genre' /> 
+            <input name='nb-places' type='number' placeholder='Nombre de places' /> 
+            <label for="date-1ere-immat">Date de la première immatriculation</label>
+            <input name='date-1ere-immat' type='date' /> 
+            <select name='type'>
+                <option value=''>--contrats--</option><?php
+                while($resContrats=$reqContrats->fetch(PDO::FETCH_ASSOC)){
+                    echo"<option value='".$resContrats['id']."'>".$resContrats['type']."</option>";
+                };?>
+
+            </select>
+            <button type='submit' value='submit' name='ajouter-vehicule' class='btn-vehicule'>Ajouter le véhicule</button>
+        </form>
                     
 
 <?php
